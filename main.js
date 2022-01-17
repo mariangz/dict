68cb560b499c986bfc474f540a8a74ebf6077fe0
mariangz <marianosgimenez@gmail.com>
2022-01-17 16:30:39 +0100

    fetch result and append them

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 const btn = document.querySelector('button');
+const result = document.getElementById('result');
 
 //input.addEventListener('input', convert);
 btn.addEventListener('click', convert);
@@ -14,17 +15,38 @@ function copy() {
 function convert() {
   const array = input.value.slice().split(' ');
   array.forEach((word) => {
-    //console.log(word)
-    const span = document.createElement('span');
-    span.classList.add('w');
-    output.appendChild(span);
-    span.textContent += ` ${word}`;
-    console.log(document.querySelector('span'));
+    if (!!word) {
+      const span = document.createElement('span');
+      span.classList.add('word');
+      output.appendChild(span);
+      span.textContent += ` ${word}`;
+      console.log(document.querySelector('span'));
+    }
   });
-  const allWords = document.querySelectorAll('w');
+  const allWords = document.querySelectorAll('.word');
+  hover(allWords);
 }
 
 function hover(array) {
-  //console.log("halo")
-  array.forEach((word) => console.log(word));
+  array.forEach((word) => {
+    word.addEventListener('click', (e) => {
+      dictionary(e.target.textContent);
+    });
+  });
+}
+
+async function dictionary(word) {
+  console.log(word);
+  const promesa = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  );
+
+  const data = await promesa.json();
+  const synonyms = data[0].meanings[0].definitions[0].synonyms;
+  synonyms.forEach((def) => {
+    const li = document.createElement('li');
+    li.textContent = def;
+    result.appendChild(li);
+  });
+  // console.log(synonyms);
 }
